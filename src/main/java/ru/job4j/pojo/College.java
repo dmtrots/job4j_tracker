8e545755073e759be8e0ed142095df4f63bb8073
Dmitriy <dmitrytrotss@gmail.com>
2024-10-01 21:33:39 +0500

    2. Модель данных. [#174683]

--- src/main/java/ru/job4j/pojo/College.java
+++ src/main/java/ru/job4j/pojo/College.java
@@ -7,8 +7,10 @@ public class College {
         student.setGroupName("ГМФ 1-2");
         student.setDateOfEnrollment("20 августа 2016 г.");
         System.out.println("ФИО студента: " + student.getFullName()
-                + "\nНазвание группы: " + student.getGroupName()
-                + "\nДата поступления: " + student.getDateOfEnrollment());
+                + System.lineSeparator()
+                + "Название группы: " + student.getGroupName()
+                + System.lineSeparator()
+                + "Дата поступления: " + student.getDateOfEnrollment());
 
     }
 }
