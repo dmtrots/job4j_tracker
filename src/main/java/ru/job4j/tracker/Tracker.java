702df4ee3f334c4b300971e05e15f40ee5c9c518
Dmitriy <dmitrytrotss@gmail.com>
2025-01-17 14:16:11 +0500

    7. Метод удаления заявки Tracker.delete [#211749]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -70,4 +70,14 @@ public class Tracker {
         }
         return result;
     }
+
+    public void delete(int id) {
+        int index = indexOf(id);
+        if (index != -1) {
+            items[indexOf(id)] = null;
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            items[size - 1] = null;
+            size--;
+        }
+    }
 }
