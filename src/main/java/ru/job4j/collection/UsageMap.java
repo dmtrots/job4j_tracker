3cc5fa150e1d8b1d561bc820aee03829090ec912
dmtrots <dmitrytrotss@gmail.com>
2025-02-05 19:19:39 +0500

    0. Map, HashMap [#212652 #531321]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,10 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("dmitrytrotss@gmail.com", "Trotsenko Dmitriy Anatolevich");
+        map.put("ivanivanov@yandex.ru", "Ivanov Ivan");
+        map.put("vvputin@mail.ru", "Vladimir Putin");
+        map.put("nevskiya@yandex.ru", "Nevskiy Alexander");
+        map.put("nevskiya@yandex.ru", "Nevskiy Alexey");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " — " + value);
