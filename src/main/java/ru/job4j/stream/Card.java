b62cd94abc8d333fb988fb17e2296805eb28edbc
dmtrots <dmitrytrotss@gmail.com>
2025-02-26 14:16:48 +0500

    9. Генерирование колоды карт. FlatMap [#504873 #533790]

--- src/main/java/ru/job4j/stream/Card.java
+++ src/main/java/ru/job4j/stream/Card.java
@@ -6,17 +6,17 @@ public class Card {
     private Suit suit;
     private Value value;
 
+    public Card(Suit suit, Value value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
     @Override
     public String toString() {
         return "Card{"
                 + "suit=" + suit
                 + ", value=" + value
                 + '}';
-    }
-
-    public Card(Suit suit, Value value) {
-        this.suit = suit;
-        this.value = value;
     }
 
     public static void main(String[] args) {
