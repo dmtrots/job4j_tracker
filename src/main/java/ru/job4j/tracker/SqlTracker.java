a383d2e29db0dd720cd4cf1a0f4cdd67c96221b0
dmtrots <dmitrytrotss@gmail.com>
2025-11-13 18:12:03 +0500

    3. Liquibase. Интеграционные тесты. [#504894 #554808]

--- src/main/java/ru/job4j/tracker/SqlTracker.java
+++ src/main/java/ru/job4j/tracker/SqlTracker.java
@@ -22,7 +22,7 @@ public class SqlTracker implements Store {
 
     private void init() {
         try (InputStream input = SqlTracker.class.getClassLoader()
-                .getResourceAsStream("db/liquibase.properties")) {
+                .getResourceAsStream("db/liquibase_test.properties")) {
             Properties config = new Properties();
             config.load(input);
             Class.forName(config.getProperty("driver-class-name"));
